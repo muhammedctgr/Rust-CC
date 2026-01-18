818d78edd21e70de711e4df0c8dcd0d8de5cd92b
unknown <muhammedko420@gmail.com>
2026-01-18 16:18:51 +0100

    pattern_match

--- topics/pattern_matching/solutions/src/lib.rs
+++ topics/pattern_matching/solutions/src/lib.rs
@@ -9,4 +9,9 @@ pub fn num_to_string(num: u32) -> String {
     .to_string()
 }
 
-
+pub fn unwrap_or_default(x: Option<u32>, v: u32) -> u32 {
+    match x {
+        Some(val) => val,
+        None => v,
+    }
+}
