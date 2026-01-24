2bca9925e1facc8c82252bf738159eb39159eeab
unknown <muhammedko420@gmail.com>
2026-01-24 13:19:42 +0100

    trait_bound

--- topics/trait_bound/solutions/examples/trait_bound.rs
+++ topics/trait_bound/solutions/examples/trait_bound.rs
@@ -28,3 +28,23 @@ where
 {
 }
 
+fn main() {
+    let u: u32 = 1;
+    let i: i32 = 1;
+    let f: f32 = 1.0;
+
+    c(u);
+    // This won't compile
+    // i32 doesn't implement A
+    // c(i);
+
+    m(u);
+    // This won't compile
+    // i32 doesn't implement A and B
+    // m(i);
+
+    w(u, i);
+    // This won't compile
+    // u32 doesn't implement C
+    // w(u, u);
+}
