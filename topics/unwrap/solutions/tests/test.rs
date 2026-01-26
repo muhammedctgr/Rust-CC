1c2e11ea2b978a7425f31739f995391cdde92f57
unknown <muhammedko420@gmail.com>
2026-01-26 10:24:53 +0100

    unwrap

--- topics/unwrap/solutions/tests/test.rs
+++ topics/unwrap/solutions/tests/test.rs
@@ -11,4 +11,10 @@ fn test_parse_and_add_panic_1() {
     parse_and_add("a", "2");
 }
 
+#[test]
+#[should_panic(expected = "Failed to parse variable")]
+fn test_parse_and_add_panic_2() {
+    parse_and_add("1", "b");
+}
+
 
