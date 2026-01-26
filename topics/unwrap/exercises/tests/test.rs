e9635b5000a25b65b7859abdfa2ba4b2a6165157
unknown <muhammedko420@gmail.com>
2026-01-26 10:21:55 +0100

    unwrap

---
+++ topics/unwrap/exercises/tests/test.rs
@@ -0,0 +1,14 @@
+use unwrap::*;
+
+#[test]
+fn test_parse_and_add() {
+    assert_eq!(parse_and_add("1", "2"), 3);
+}
+
+#[test]
+#[should_panic(expected = "Failed to parse variable")]
+fn test_parse_and_add_panic_1() {
+    parse_and_add("a", "2");
+}
+
+
